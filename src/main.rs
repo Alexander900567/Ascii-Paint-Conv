b249e7dc83cdda3e6c908d2d59adae929154dc17
alex <alexanderkershen@gmail.com>
2024-09-21 20:01:08 -0500

    breif sdl initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let window_width: u32 = 600;
+    let window_height: u32 = 800;
+
+    let sdl_context = sdl2::init().expect("failed to init sdl");
+    let video_subsystem = sdl_context.video().expect("failed to init video subsytem");
+
+    let window = video_subsystem.window("ascii", window_width, window_height)
+        .position_centered()
+        .build()
+        .expect("failed to build window");
+
 }
+
+
