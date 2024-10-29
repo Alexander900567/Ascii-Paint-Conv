44dcc57b6f83ef7da2dec2596898fdfde0c8d1e7
FyreAxis <xxcomedyskitzxx@gmail.com>
2024-10-29 01:59:15 -0500

    Creates new naming canon for tools. Main will follow.

--- src/main.rs
+++ src/main.rs
@@ -20,33 +20,33 @@ fn line_tool(main_window: &mut main_window::MainWindow<'_>,
         main_window.preview_buffer.clear();
     }
 
-    let mut x_slope = fin_row - begin_row;
-    let mut y_slope = fin_col - begin_col;
-    let mut x_iter = 0;
-    let mut y_iter = 0;
+    let mut horizontal_slope = fin_row - begin_row;
+    let mut vertical_slope = fin_col - begin_col;
+    let mut row_iter = 0;
+    let mut col_iter = 0;
 
-    if x_slope != 0 {
-        x_iter = x_slope / x_slope.abs(); 
+    if horizontal_slope != 0 {
+        row_iter = horizontal_slope / horizontal_slope.abs(); 
     }
-    if y_slope != 0 {
-        y_iter = y_slope / y_slope.abs(); 
+    if vertical_slope != 0 {
+        col_iter = vertical_slope / vertical_slope.abs(); 
     }
 
-    x_slope = x_slope.abs();
-    y_slope = y_slope.abs();
+    horizontal_slope = horizontal_slope.abs();
+    vertical_slope = vertical_slope.abs();
 
     let long_slope;
     let short_slope;
-    let x_is_long;
-    if x_slope > y_slope {
-        long_slope = x_slope;
-        short_slope = y_slope + 1;
-        x_is_long = true;
+    let row_length_is_long;
+    if horizontal_slope > vertical_slope {
+        long_slope = horizontal_slope;
+        short_slope = vertical_slope + 1;
+        row_length_is_long = true;
     }
     else {
-        long_slope = y_slope;
-        short_slope = x_slope + 1;
-        x_is_long = false;
+        long_slope = vertical_slope;
+        short_slope = horizontal_slope + 1;
+        row_length_is_long = false;
     }
 
     let per_chunk = long_slope / short_slope;
@@ -60,18 +60,18 @@ fn line_tool(main_window: &mut main_window::MainWindow<'_>,
         }
         for _ in 0..this_chunk {
             main_window.add_to_preview_buffer(begin_row, begin_col);    
-            if x_is_long {
-                begin_row += x_iter;   
+            if row_length_is_long {
+                begin_row += row_iter;   
             }
             else {
-                begin_col += y_iter;
+                begin_col += col_iter;
             }
         }
-        if !x_is_long {
-            begin_row += x_iter;   
+        if !row_length_is_long {
+            begin_row += row_iter;   
         }
         else{
-            begin_col += y_iter;
+            begin_col += col_iter;
         }
     }
 } //commit changes after run
@@ -107,22 +107,22 @@ fn filled_rectangle_tool(main_window: &mut main_window::MainWindow<'_>, current_
     let fin_row: i32 = current_mouse_pos[0];
     let fin_col: i32 = current_mouse_pos[1];
     
-    let leftx:i32;
-    let rightx:i32;
+    let leftmost_row:i32;
+    let rightmost_row:i32;
 
     if begin_row <= fin_row { //right quadrants case
-        leftx = begin_row;
-        rightx = fin_row;
+        leftmost_row = begin_row;
+        rightmost_row = fin_row;
     }
     else { //left quadrants case
-        leftx = fin_row;
-        rightx = begin_row;
+        leftmost_row = fin_row;
+        rightmost_row = begin_row;
     }
 
-    for x in leftx..=rightx { //iterates vertical lines
+    for row_num in leftmost_row..=rightmost_row { //iterates vertical lines
         line_tool(main_window,
-        &[x, begin_col], //further iterates those lines horizontally (left to right or right to left)
-        &[x, fin_col],
+        &[row_num, begin_col], //further iterates those lines horizontally (left to right or right to left)
+        &[row_num, fin_col],
         false);
     }
 }
@@ -140,18 +140,18 @@ fn circle_tool(main_window: &mut main_window::MainWindow<'_>,
     let begin_col: i32 = start_mouse_pos[1];
     let fin_col: i32 = current_mouse_pos[1];
 
-    let x_component:i32 = fin_row - begin_row;
-    let y_component:i32 = fin_col - begin_col;
+    let row_dif:i32 = fin_row - begin_row;
+    let col_dif:i32 = fin_col - begin_col;
     let r:i32;
-    let diagonal_r:f32 = f32::sqrt((x_component as f32 * x_component as f32) + (y_component as f32 * y_component as f32)); //pythag h
+    let diagonal_r:f32 = f32::sqrt((row_dif as f32 * row_dif as f32) + (col_dif as f32 * col_dif as f32)); //pythag h
     //theory: given r = 10
     /* diagonal_r = real hypotenuse = 10*sqrt(2) = r*ratio
     r (radius of circle)= diagonal_r / ratio
     ratio = hypotenuse of a triangle with sides divided by r with same angle theta = h = (o/r)/sin(theta) 
     theta = sin^-1(o/diagonal_r) */
-    match(x_component, y_component) {
-        (x,y) if x != 0 && y != 0 => { //non-cardinal case
-        let o:i32 = y_component.abs(); //to keep scalar factor positive (since we're about to use sin)
+    match(row_dif, col_dif) {
+        (row_num,col_num) if row_num != 0 && col_num != 0 => { //non-cardinal case
+        let o:i32 = col_dif.abs(); //to keep scalar factor positive (since we're about to use sin)
         let angle_theta:f32 = f32::asin(o as f32/diagonal_r);
         let h:f32 = (o as f32/diagonal_r as f32)/f32::sin(angle_theta);
         let r0: f32 = diagonal_r/h;
@@ -168,27 +168,27 @@ fn circle_tool(main_window: &mut main_window::MainWindow<'_>,
         _ => r = 0 //catch all
     }
 
-    let mut x:i32 = 0i32;
-    let mut y:i32 = r; //(x,y) = (0,r)
+    let mut row_num:i32 = 0i32;
+    let mut col_num:i32 = r; //(row_num,col_num) = (0,r)
     let mut p:i32 = 1 - r;
 
-    while x <= y {
-        main_window.add_to_preview_buffer(begin_row + x, begin_col + y);
-        main_window.add_to_preview_buffer(begin_row + y, begin_col + x);
-        main_window.add_to_preview_buffer(begin_row - y, begin_col + x);
-        main_window.add_to_preview_buffer(begin_row - x, begin_col + y);
-        main_window.add_to_preview_buffer(begin_row - x, begin_col - y);
-        main_window.add_to_preview_buffer(begin_row - y, begin_col - x);
-        main_window.add_to_preview_buffer(begin_row + y, begin_col - x);
-        main_window.add_to_preview_buffer(begin_row + x, begin_col - y);
+    while row_num <= col_num {
+        main_window.add_to_preview_buffer(begin_row + row_num, begin_col + col_num);
+        main_window.add_to_preview_buffer(begin_row + col_num, begin_col + row_num);
+        main_window.add_to_preview_buffer(begin_row - col_num, begin_col + row_num);
+        main_window.add_to_preview_buffer(begin_row - row_num, begin_col + col_num);
+        main_window.add_to_preview_buffer(begin_row - row_num, begin_col - col_num);
+        main_window.add_to_preview_buffer(begin_row - col_num, begin_col - row_num);
+        main_window.add_to_preview_buffer(begin_row + col_num, begin_col - row_num);
+        main_window.add_to_preview_buffer(begin_row + row_num, begin_col - col_num);
 
-        x += 1; //all 4 regions
+        row_num += 1; //all 4 regions
         if p < 0 {
-            p += 2 * x + 1
+            p += 2 * row_num + 1
         }
         else {
-            y -= 1;
-            p += 2 * (x - y) + 1;
+            col_num -= 1;
+            p += 2 * (row_num - col_num) + 1;
         }
     }    
 }
@@ -203,14 +203,14 @@ fn filled_circle_tool(main_window: &mut main_window::MainWindow<'_>, current_mou
     let begin_col: i32 = start_mouse_pos[1];
     let fin_col: i32 = current_mouse_pos[1];
 
-    let x_component:i32 = fin_row - begin_row;
-    let y_component:i32 = fin_col - begin_col;
+    let row_dif:i32 = fin_row - begin_row;
+    let col_dif:i32 = fin_col - begin_col;
     let r:i32;
-    let diagonal_r:f32 = f32::sqrt((x_component as f32 * x_component as f32) + (y_component as f32 * y_component as f32));
+    let diagonal_r:f32 = f32::sqrt((row_dif as f32 * row_dif as f32) + (col_dif as f32 * col_dif as f32));
 
-    match(x_component, y_component) {
-        (x,y) if x != 0 && y != 0 => { //non-cardinal case
-        let o:i32 = y_component.abs(); //to keep scalar factor positive (since we're about to use sin)
+    match(row_dif, col_dif) {
+        (row_num,col_num) if row_num != 0 && col_num != 0 => { //non-cardinal case
+        let o:i32 = col_dif.abs(); //to keep scalar factor positive (since we're about to use sin)
         let angle_theta:f32 = f32::asin(o as f32/diagonal_r);
         let h:f32 = (o as f32/diagonal_r as f32)/f32::sin(angle_theta);
         let r0: f32 = diagonal_r/h;
@@ -227,39 +227,39 @@ fn filled_circle_tool(main_window: &mut main_window::MainWindow<'_>, current_mou
         _ => r = 0 //catch all
     }
 
-    let mut x = 0i32;
-    let mut y = r;
+    let mut row_num = 0i32;
+    let mut col_num = r;
     let mut p = 1 - r; //haven't assigned r, assign later
 
-    while x <= y {
+    while row_num <= col_num {
 
         line_tool(main_window,
-        &[(begin_row + x), (begin_col + y)], 
-        &[(begin_row - x), (begin_col + y)],
+        &[(begin_row + row_num), (begin_col + col_num)], 
+        &[(begin_row - row_num), (begin_col + col_num)],
         false);
 
         line_tool(main_window,
-        &[(begin_row + y), (begin_col + x)], 
-        &[(begin_row - y), (begin_col + x)],
+        &[(begin_row + col_num), (begin_col + row_num)], 
+        &[(begin_row - col_num), (begin_col + row_num)],
         false);
 
         line_tool(main_window,
-        &[(begin_row + x), (begin_col - y)], 
-        &[(begin_row - x), (begin_col - y)],
+        &[(begin_row + row_num), (begin_col - col_num)], 
+        &[(begin_row - row_num), (begin_col - col_num)],
         false);
 
         line_tool(main_window,
-        &[(begin_row + y), (begin_col - x)], 
-        &[(begin_row - y), (begin_col - x)],
+        &[(begin_row + col_num), (begin_col - row_num)], 
+        &[(begin_row - col_num), (begin_col - row_num)],
         false);
 
-        x += 1;
+        row_num += 1;
         if p < 0 {
-            p += 2 * x + 1;
+            p += 2 * row_num + 1;
         }
         else {
-            y -= 1;
-            p += 2 * (x - y) + 1;
+            col_num -= 1;
+            p += 2 * (row_num - col_num) + 1;
         }
     }
 }
@@ -275,49 +275,49 @@ fn draw_ellipse<F>(main_window: &mut main_window::MainWindow<'_>, mut render_fun
     let begin_col: i32 = start_mouse_pos[1];
     let fin_col: i32 = current_mouse_pos[1];
 
-    let x_component:i32 = (fin_row - begin_row).abs();
-    let y_component:i32 = (fin_col - begin_col).abs();
-    let x_squared: f32 = (x_component * x_component) as f32;
-    let y_squared: f32 = (y_component * y_component) as f32;
+    let row_dif:i32 = (fin_row - begin_row).abs();
+    let col_dif:i32 = (fin_col - begin_col).abs();
+    let row_diff_squared: f32 = (row_dif * row_dif) as f32;
+    let col_diff_squared: f32 = (col_dif * col_dif) as f32;
 
-    let mut x: i32 = 0;
-    let mut y: i32 = y_component;
+    let mut row_num: i32 = 0;
+    let mut col_num: i32 = col_dif;
 
-    let mut px:f32 = 0.0;
-    let mut py:f32 = 2.0 * x_squared * y as f32;
+    let mut p_row:f32 = 0.0;
+    let mut p_col:f32 = 2.0 * row_diff_squared * col_num as f32;
 
-    render_func(main_window, begin_row, begin_col, x, y);
+    render_func(main_window, begin_row, begin_col, row_num, col_num);
 
     //Top and bottom
-    let mut p:f32 = y_squared - (x_squared * y_component as f32) + (0.25f32 * x_squared);
-    while px <= py {
-        x += 1;
-        px += 2.0 * y_squared;
+    let mut p:f32 = col_diff_squared - (row_diff_squared * col_dif as f32) + (0.25f32 * row_diff_squared);
+    while p_row <= p_col {
+        row_num += 1;
+        p_row += 2.0 * col_diff_squared;
         if p < 0.0 {
-            p += y_squared + px;
+            p += col_diff_squared + p_row;
         }
         else {
-            y -= 1;
-            py += -2.0 * x_squared;
-            p += y_squared + px - py;
+            col_num -= 1;
+            p_col += -2.0 * row_diff_squared;
+            p += col_diff_squared + p_row - p_col;
         }
-        render_func(main_window, begin_row, begin_col, x, y);
+        render_func(main_window, begin_row, begin_col, row_num, col_num);
     }
 
     //Left and right
-    p = (y_squared * ((x as f32 + 0.5).powi(2))) + (x_squared * (y - 1).pow(2) as f32) - (x_squared * y_squared);
-    while y >= 0 {
-        y -= 1;
-        py += -2.0 * x_squared;
+    p = (col_diff_squared * ((row_num as f32 + 0.5).powi(2))) + (row_diff_squared * (col_num - 1).pow(2) as f32) - (row_diff_squared * col_diff_squared);
+    while col_num >= 0 {
+        col_num -= 1;
+        p_col += -2.0 * row_diff_squared;
         if p > 0.0 {
-            p += x_squared - py;
+            p += row_diff_squared - p_col;
         }
         else {
-            x += 1;
-            px += (2.0 * y_squared);
-            p += x_squared - py + px;
+            row_num += 1;
+            p_row += (2.0 * col_diff_squared);
+            p += row_diff_squared - p_col + p_row;
         }
-        render_func(main_window, begin_row, begin_col, x, y);
+        render_func(main_window, begin_row, begin_col, row_num, col_num);
     }
 }
 
@@ -331,39 +331,37 @@ fn ellipse_tool(main_window: &mut main_window::MainWindow<'_>, current_mouse_pos
     let begin_col: i32 = start_mouse_pos[1]; //i guess you're right, but it doesn't change what's going on lol
     let fin_col: i32 = current_mouse_pos[1]; //fair
 
-    let x_component:i32 = (fin_row - begin_row).abs(); //and these
-    let y_component:i32 = (fin_col - begin_col).abs();
+    let row_dif:i32 = (fin_row - begin_row).abs(); //and these
+    let col_dif:i32 = (fin_col - begin_col).abs();
     // Circle is faster, so do not waste time using this tool if it's a circle
-    if x_component == y_component {
+    if row_dif == col_dif {
         circle_tool(main_window,
         &[begin_row, begin_col],
         &[fin_row, fin_col]);
         return;
     }
-    else if y_component == 0 { //that's why these were flipped. they're misnamed
-        println!("We entered the statement"); //these will make normal lines now, but only that
-        line_tool(main_window,
-        &[begin_row + (-2 * (begin_row - fin_row)), begin_col], //does this feel weird to you too? like these may be wrong, but they aren't displaying what they should, right?
-        &[begin_row, begin_col], //didn't matter, they should both be the same. since x_comp = 0, both x are same
-        true); //i've been messing with both lol
-        return; //these should both be straight lines.
+    else if col_dif == 0 { //Straight line is faster
+    line_tool(main_window,
+    &[begin_row + (-2 * (begin_row - fin_row)), begin_col],
+    &[begin_row, begin_col],
+    true);
+    return; //only do straight line, no ellipse at all
     }
-    else if x_component == 0 { //oh it'll work fine if you do this, but change ANYTHING, and it goes bonkers
-        println!("We entered the other statement");
-        line_tool(main_window,
-        &[fin_row, begin_col + (-2 * (begin_col - fin_col))],
-        &[begin_row, begin_col],
-        false); //this should be futureproofed when we decide to do odd ellipses
-        return;  
+    else if row_dif == 0 { 
+    line_tool(main_window,
+    &[fin_row, begin_col + (-2 * (begin_col - fin_col))],
+    &[begin_row, begin_col],
+    true); 
+    return;  
     }
     //passed to draw_ellipse
-    let draw_quad_pixels = |main_window: &mut main_window::MainWindow<'_>, begin_row: i32, begin_col: i32, x: i32, y: i32| {
+    let draw_quad_pixels = |main_window: &mut main_window::MainWindow<'_>, begin_row: i32, begin_col: i32, row_num: i32, col_num: i32| {
         //mentioned in previous credits's source, but I figured I'd be specific https://web.archive.org/web/20160128020853/http://tutsheap.com/c/mid-point-ellipse-drawing-algorithm/
         //draw_quad_pixels in doc.rs
-        main_window.add_to_preview_buffer(begin_row + x, begin_col + y);
-        main_window.add_to_preview_buffer(begin_row - x, begin_col + y);
-        main_window.add_to_preview_buffer(begin_row + x, begin_col - y);
-        main_window.add_to_preview_buffer(begin_row - x, begin_col - y);
+        main_window.add_to_preview_buffer(begin_row + row_num, begin_col + col_num);
+        main_window.add_to_preview_buffer(begin_row - row_num, begin_col + col_num);
+        main_window.add_to_preview_buffer(begin_row + row_num, begin_col - col_num);
+        main_window.add_to_preview_buffer(begin_row - row_num, begin_col - col_num);
     };
 
     draw_ellipse(main_window,
@@ -382,27 +380,27 @@ fn filled_ellipse_tool(main_window: &mut main_window::MainWindow<'_>, current_mo
     let begin_col: i32 = start_mouse_pos[1];
     let fin_col: i32 = current_mouse_pos[1];
 
-    let x_component:i32 = (fin_row - begin_row).abs();
-    let y_component:i32 = (fin_col - begin_col).abs();
+    let row_dif:i32 = (fin_row - begin_row).abs();
+    let col_dif:i32 = (fin_col - begin_col).abs();
 
     //same as above tool, circle will be faster
-    if x_component == y_component {
+    if row_dif == col_dif {
         filled_circle_tool(main_window,
         &[begin_row, begin_col],
         &[fin_row, fin_col]);
         return;
     }
     //will be passed to draw_ellipse to draw line pair when drawing
-    let draw_line_pairs = |main_window: &mut main_window::MainWindow<'_>, begin_row: i32, begin_col: i32, x: i32, y: i32| {
+    let draw_line_pairs = |main_window: &mut main_window::MainWindow<'_>, begin_row: i32, begin_col: i32, row_num: i32, col_num: i32| {
         line_tool(
             main_window,
-            &[(begin_row - x), (begin_col + y)],
-            &[(begin_row + x), (begin_col + y)],
+            &[(begin_row - row_num), (begin_col + col_num)],
+            &[(begin_row + row_num), (begin_col + col_num)],
             false );
         line_tool(
             main_window,
-            &[(begin_row - x), (begin_col - y)],
-            &[(begin_row + x) , (begin_col - y)],
+            &[(begin_row - row_num), (begin_col - col_num)],
+            &[(begin_row + row_num) , (begin_col - col_num)],
             false );
     };
 
